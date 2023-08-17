ec90fc5da875b59f2a18efc01d7c05567c183ae0
Lam Thang <skydark.304@gmail.com>
2023-08-17 13:24:36 +0700

    env

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,7 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    console.log(111);
-    return 'Hello World! 123';
+    return `Hello World! ${process.env.NODE_ENV}`;
   }
 }
